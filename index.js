69b963073655f45d89ec0075cac87a9acd18f3c0
LutLas <lutakachihota@gmail.com>
2025-08-13 12:51:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { ApolloServer } = require("apollo-server");
+const neo4j = require("neo4j-driver");
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+
+const typeDefs = /* GraphQL */ `
+    type Business @node{
+        businessId: ID!
+        name: String!
+        city: String!
+        state: String!
+        address: String!
+        location: Point!
+        reviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
+        categories: [Category!]! @relationship(type: "IN_CATEGORY", direction: OUT)
+    }
+
+    type User @node{
+        userID: ID!
+        name: String!
+        reviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
+    }
+
+    type Review @node{
+        reviewId: ID!
+        stars: Float!
+        date: Date!
+        text: String
+        user: User! @relationship(type: "WROTE", direction: IN)
+        business: Business! @relationship(type: "REVIEWS", direction: OUT)
+    }
+
+    type Category @node{
+        name: String!
+        businesses: [Business!]! @relationship(type: "IN_CATEGORY", direction: IN)
+    }
+`;
+
+const driver = neo4j.driver(
+    "bolt://localhost:7687",
+    neo4j.auth.basic("neo4j", "password")
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver});
+
+neoSchema.getSchema().then((schema) => {
+    const server = new ApolloServer({
+        schema,
+    });
+    server.listen().then(({ url }) => {
+        console.log(`GraphQL server ready at ${url}`);
+    });
+});
